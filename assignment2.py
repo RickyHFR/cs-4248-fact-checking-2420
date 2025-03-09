92a750b64b78c77532ab88789378cdb084e837da
RickyHFR <huangfengr@gmail.com>
2025-03-09 16:58:16 +0800

    import libs

--- assignment2.py
+++ assignment2.py
@@ -21,6 +21,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.pipeline import Pipeline
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
+from gensim.models import Word2Vec
 
 nltk.download('wordnet')
 nltk.download('stopwords')
